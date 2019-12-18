dd51283430bcf9841c45c7c69e39e89074f177a3
jinghuazhao <jinghuazhao@hotmail.com>
2019-12-18 16:19:54 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -1,8 +1,8 @@
-# 1-8-2018 JHZ
+# 17-12-2019 JHZ
 
 git add README.md
 git commit -m "README"
-for d in AF BMI CAD T2D st.sh
+for d in eQTL AF BMI CAD T2D st.sh
 do
    git add $d
    git commit -m "$d"
