ada342957bbe6ee8dd77ad8b83a0db51d5ec6996
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-09-11 14:17:42 +0100

    files

--- files/st.sh
+++ files/st.sh
@@ -1,8 +1,8 @@
-# 17-12-2019 JHZ
+# 11-9-2020 JHZ
 
 git add README.md
 git commit -m "README"
-for d in eQTL AF BMI CAD T2D st.sh
+for d in eQTL AF BMI CAD T2D files
 do
    git add $d
    git commit -m "$d"
