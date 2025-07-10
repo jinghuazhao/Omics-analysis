913ef638281b4b8cee3e2483b4c8656f759a05bb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-07-10 17:46:31 +0100

    Deployed b60a37e with MkDocs version: 1.5.3

---
+++ files/msc.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+# https://crux.ms/fileformats.html
+
+module load ceuadmin/wine/8.21
+module load ceuadmin/pwiz/3_0_24163_9bfa69a-wine
+export raw=ScltlMsclsMAvsCntr_Batch1_BRPhsFr5.raw
+
+singularity --version
+export crux=/rds/project/rds-zuZwCZMsS0w/Caprion_proteomics/analysis/crux
+export SIF=pwiz-skyline-i-agree-to-the-vendor-licenses_latest.sif
+ln -s $crux/$SIF
+function sif()
+{
+   singularity pull --name ${SIF} docker://chambm/pwiz-skyline-i-agree-to-the-vendor-licenses
+ # http://localhost:8080, Admin, password
+   ln -sf /rds/project/rds-4o5vpvAowP0/software/.apptainer/ ${HOME}/.apptainer
+   singularity pull docker://quay.io/galaxy/introduction-training
+}
+for format in --mzML
+do
+singularity exec --env WINEDEBUG=-all \
+                  -B /rds/project/rds-C1Ph08tkaOA/public/ms-proteomics/MSV000086195/raw:/data \
+                      ${crux}/${SIF} \
+                      wine msconvert ${format} /data/${raw}
+done
+# --mzXML --mz5 --mzMLb --mgf --text --ms1 --cms1 --ms2 --cms2
